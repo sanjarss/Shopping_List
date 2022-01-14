d9e8eba98c2068a0e08b24ca8a6ce001e792ff95
sanjarss <sanzhar.satymbaev@gmail.com>
2022-01-14 16:13:50 +0600

    first commit

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemAction';
+import PropTypes from 'prop-types'; 
+
+class ShoppingList extends Component {
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+
+    onDeleteClick = (id) => {
+        this.props.deleteItem(id)
+        console.log(id, "clicked id");
+    }
+
+    render() {
+        const {items} = this.props.item;
+
+        return(
+            <Container>
+                    <ListGroup>
+                        <TransitionGroup className='shopping-list'>
+                            {items.map(({_id, name}) => (
+                                <CSSTransition key={_id} timeout={5000} classNames='fade'>
+                                    <ListGroupItem>
+                                        <Button
+                                            className='remove-btn'
+                                            color='danger'
+                                            size='sm'
+                                            onClick={this.onDeleteClick.bind(this, _id)}
+                                        >&times;</Button>
+                                        {name}
+                                    </ListGroupItem>
+                                </CSSTransition>
+                            ))}
+                        </TransitionGroup>
+                    </ListGroup>
+            </Container>
+        );
+    }
+
+}
+
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+})
+
+export default connect(
+    mapStateToProps,
+    { getItems, deleteItem })
+    (ShoppingList);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://sanzhar:188200Sr@mern-shopping.yoz6h.mongodb.net/mern-shopping?retryWrites=true&w=majority'
+}
